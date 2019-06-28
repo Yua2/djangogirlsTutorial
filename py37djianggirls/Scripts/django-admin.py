5f320a4a233cbc22e0fd2a80c8a5406e4f6505f0
godify <hero_kanenyu_bynx2maru@yahoo.co.jp>
2019-06-28 12:09:59 +0900

    first commit

---
+++ py37djianggirls/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\sugi8\documents\python_project\django_girls\py37djianggirls\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
