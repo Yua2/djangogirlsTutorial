0fe9e496039dc415de0bf8ea52332ff93fe0deb2
godify <hero_kanenyu_bynx2maru@yahoo.co.jp>
2019-07-07 12:18:44 +0900

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+
+def okada(request):
+    return render(request, 'blog/okada.html', {})
+
